6d11a462831fac25ff8a53a12628a7a5f422a16b
Benxodia <anasbenzougagh@gmail.com>
2023-11-06 13:37:17 -0800

    done

--- src/test/java/com/utilities/API_Util.java
+++ src/test/java/com/utilities/API_Util.java
@@ -1,21 +1,12 @@
 package com.utilities;
 
-import com.github.javafaker.Faker;
 import io.restassured.http.ContentType;
-
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 
 public class API_Util {
 
 
-    /**
-     * Return TOKEN as String by using provided username from /token endpoint
-     * @param userType
-     * @return
-     */
     public static String getToken(String userType){
 
         String email=ConfigurationReader.getProperty(userType+"_username");
@@ -34,14 +25,5 @@ public class API_Util {
                 .post(ConfigurationReader.getProperty("baseUri")+"/login")
                 .prettyPeek()
                 .path("token") ;
-    }
-
-    public static Map<String,Object> getRandomDataMap(){
-
-        Faker faker = new Faker() ;
-        Map<String,Object> bookMap = new LinkedHashMap<>();
-        bookMap.put("address", faker.address());
-        //...
-        return bookMap ;
     }
 }
