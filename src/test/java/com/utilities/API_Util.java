acfd98092a78aa19afd7dff47b75277c2f4a4466
Benxodia <anasbenzougagh@gmail.com>
2024-04-17 12:42:34 -0700

    done

--- src/test/java/com/utilities/API_Util.java
+++ src/test/java/com/utilities/API_Util.java
@@ -1,29 +1,4 @@
 package com.utilities;
 
-import io.restassured.http.ContentType;
-
-import static io.restassured.RestAssured.given;
-
 public class API_Util {
-
-
-    public static String getToken(String userType){
-
-        String email=ConfigurationReader.getProperty(userType+"_username");
-        String password="";
-        
-        return getToken(email,password);
-    }
-
-    public static String getToken(String email,String password){
-
-        return given()
-                .contentType(ContentType.URLENC)
-                .formParam("email" , email)
-                .formParam("password" , password).
-                when()
-                .post(ConfigurationReader.getProperty("baseUri")+"/login")
-                .prettyPeek()
-                .path("token") ;
-    }
 }
